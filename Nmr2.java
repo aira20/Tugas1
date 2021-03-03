4355d247039995bebf1b91e4a67c5d9407b34b68
Aira <63812207+aira20@users.noreply.github.com>
2021-03-03 11:11:13 +0700

    Update Nmr2.java

--- Nmr2.java
+++ Nmr2.java
@@ -12,51 +12,49 @@ import java.util.Scanner;
  *
  * @author Aria
  */
-public class Nmr2 {
+public class Nmr2 
+{
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
 int  days, distance;
 
 Scanner read = new Scanner(System.in);
-        System.out.println("Hari ini adalah hari senin ");
+System.out.println("Hari ini adalah hari senin ");
         
-        System.out.print("Masukan distance hari ke depan : ");
-         days = read.nextInt();
-        
-        distance =  days % 7;
+System.out.print("Masukan distance hari ke depan : ");
+days = read.nextInt(); distance =  days % 7;
         
         if (distance == 0)
 {
-            System.out.println("Maka " + days + " hari berikutnya adalah hari Senin");
+System.out.println("Maka " + days + " hari berikutnya adalah hari Senin");
         }
         else if (distance == 1)
 {
-            System.out.println("Maka " + days + " hari berikutnya adalah hari Selasa");
+System.out.println("Maka " + days + " hari berikutnya adalah hari Selasa");
         }
         else if (distance == 2)
 {
-            System.out.println("Maka " + days + " hari berikutnya adalah hari Rabu");
+System.out.println("Maka " + days + " hari berikutnya adalah hari Rabu");
         }
         else if (distance == 3)
 {
-            System.out.println("Maka " + days + " hari berikutnya adalah hari Kamis");
+System.out.println("Maka " + days + " hari berikutnya adalah hari Kamis");
         }
         else if (distance == 4)
 {
-            System.out.println("Maka " + days + " hari berikutnya adalah hari Jumat");
+System.out.println("Maka " + days + " hari berikutnya adalah hari Jumat");
         }
         else if (distance == 5)
-{
-            System.out.println("Maka " + days + " hari berikutnya adalah hari Sabtu");
+{System.out.println("Maka " + days + " hari berikutnya adalah hari Sabtu");
         }
         else if (distance == 6)
 {
-            System.out.println ("Maka " + days + " hari berikutnya adalah hari Minggu");
+System.out.println ("Maka " + days + " hari berikutnya adalah hari Minggu");
         }
-
-    }
+ }
 
 }
